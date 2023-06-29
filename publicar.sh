11abdd31ab5ead7d200ea1c4a0383de986ba5ea5
Victor on Alfara <victor@on-alfara.com>
2023-06-29 13:05:18 +0200

    Script para publicar mejorado

--- publicar.sh
+++ publicar.sh
@@ -2,5 +2,25 @@
 
 file_ics=~/Dropbox/Apps/Orgzly/agenda.ics
 
-cp $file_ics .
+# Copiar el archivo al repositorio local
+cp "$file_ics" .
+
+# Verificar si la red WiFi es eduroam
+current_wifi=$(iwgetid -r)
+if [[ $current_wifi == "eduroam" ]]; then
+    echo "No se puede realizar el push desde la red WiFi 'eduroam'. Cambia a otra red para poder hacerlo."
+    exit 1
+fi
+
+# Obtener la hora y el minuto actual
+current_time=$(date +"%H:%M")
+
+# Realizar el commit con la hora y el minuto actual
+commit_message="Incorporar nueva agenda a las $current_time"
+git add agenda.ics
+git commit -m "$commit_message"
+
+# Hacer push al repositorio remoto
+git push origin main
+
 
